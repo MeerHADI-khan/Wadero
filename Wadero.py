0ac462d1a876e2d3097891d08c8748ad92c52f58
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-20 17:34:56 +0500

    Wadero.py
    - `pkg update`
    - `pkg upgrade`
    - `pkg install git`
    - `pkg install python`
    - `pip install requests`
    - `pip install mechanize`
    - `pip install bs4`
    - `rm -rf Wadero`
    - `git clone --depth=1 https://github.com/MeerHADI-khan/Wadero.Git`
    - `cd Wadero`
    - `python Wadero.py

---
+++ Wadero.py
@@ -0,0 +1,32 @@
+# MR-HADI
+PAID TOOLS
+<b></b> </br> <br>[![Github](https://img.shields.io/badge/Github-MR.AKING-dimgray?style=flat-square&logo=github)](https://github.com/MeerHADI-khan/Wadero.Git)<br> [![Facebook](https://img.shields.io/badge/Facebook-AKING-blue?style=flat-square&logo=facebook)](https://www.facebook.com/profile.php?id=100076381339868)<br> [![Whatsapp](https://img.shields.io/badge/Whatsapp-AKING-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
+
+
+
+<h1 align="center"> [MR-HADI.-]</h1>
+
+<h2 align="center">  FB CLONING TOOL </h2>
+
+
+## <b>installation</b>
+
+🔰 _CLONE FULL OK IDZ_
+
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install git`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install bs4`
+- `rm -rf Wadero`
+- `git clone --depth=1 https://github.com/MeerHADI-khan/Wadero.Git`
+- `cd Wadero`
+- `python Wadero.py`
+
+
+
+ ___This Tools is Paid___</br>
+ [![Whatsapp](https://img.shields.io/badge/Whatsapp-MR-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
